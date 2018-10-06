58fcbaacfad501cb15265432f4ce781bfa38c65f
Fine-ants <jordanhuusy@live.com>
2018-10-06 09:04:39 -0700

    Added Board class

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -1,6 +1,29 @@
 package com.company;
 
 public class Board{
-    String[] values;
+    public Board() {
+    }
 
+
+    public void solve(Square[] squares){
+        boolean squaresUnsolved = true;
+
+        while(squaresUnsolved) {
+
+            // Retrieve answers for each square
+            for (Square square : squares) {
+
+            }
+
+            // Check each square for one solution
+            for(Square square : squares) {
+                if (!square.isSolved()) {
+                    squaresUnsolved = true;
+                    break;
+                }else{
+                    squaresUnsolved = false;
+                }
+            }
+        }
+    }
 }
