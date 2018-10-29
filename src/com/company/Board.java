8e4621a8ef7312acc5a601d854ce1bae1cabea13
Fine-ants <jordanhuusy@live.com>
2018-10-29 13:05:24 -0700

    Adding DFS utility function

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -3,8 +3,9 @@ package com.company;
 import org.apache.commons.lang3.ArrayUtils;
 
 import java.util.HashMap;
+import java.util.Iterator;
 
-public class Board{
+public class Board {
 
     private Integer[][] puzzleNums;
     private HashMap<String, Cell> cells;
@@ -12,6 +13,7 @@ public class Board{
 
     /**
      * Construct a Board object in order to store and solve the array of Cell objects
+     *
      * @param puzzle int[][] puzzle array; [row][column]; zero equals blank
      */
     public Board(Integer[][] puzzle) {
@@ -24,15 +26,16 @@ public class Board{
 
     /**
      * Print the Sudoku board
+     *
      * @return string representation of the Sudoku board
      */
     @Override
     public String toString() {
-        String boardString="";
-        for(int row=0; row<9; row++) {
+        String boardString = "";
+        for (int row = 0; row < 9; row++) {
             for (int col = 0; col < 9; col++) {
                 // Print each cell
-                boardString += puzzleNums[row][col] == 0 ? " |" : puzzleNums[row][col]+"|";
+                boardString += puzzleNums[row][col] == 0 ? " |" : puzzleNums[row][col] + "|";
             }
             boardString += "\n";
         }
@@ -42,6 +45,34 @@ public class Board{
 
 
     public void solve() {
+
+        // DFS
+
+    }
+
+    // A function used by DFS
+    void DFSUtil(int v, boolean visited[]) {
+        // Mark the current node as visited and print it
+        visited[v] = true;
+        System.out.print(v + " ");
+
+        // Recur for all the vertices adjacent to this vertex
+        Iterator<Integer> i = adj[v].listIterator();
+        while (i.hasNext()) {
+            int n = i.next();
+            if (!visited[n])
+                DFSUtil(n, visited);
+        }
+    }
+
+    // The function to do DFS traversal. It uses recursive DFSUtil()
+    void DFS(int v) {
+        // Mark all the vertices as not visited(set as
+        // false by default in java)
+        boolean visited[] = new boolean[V];
+
+        // Call the recursive helper function to print DFS traversal
+        DFSUtil(v, visited);
     }
 }
 
