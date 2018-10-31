0cdf1b0b6f62fd9b82de9a08ea694a5a9b884085
Fine-ants <jordanhuusy@live.com>
2018-10-30 19:34:11 -0700

    debugging validateBoard()

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 
 public class Board {
 
-    private Integer[][] puzzleNums;
+    private final Integer[][] puzzleNums;
     private HashMap<String, Cell> cells;
 
 
@@ -45,8 +45,7 @@ public class Board {
 
 
     public void solve() {
-
-        validateBoard();
+        System.out.println(validateBoard());
 
     }
 
@@ -57,7 +56,8 @@ public class Board {
             Integer[] sorted = puzzleNums[row];
             Arrays.sort(sorted);
             sorted = ArrayUtils.removeAllOccurences(sorted, 0);
-            for(int i=1; i<=9; i++){
+            System.out.println(Arrays.toString(sorted));
+            for(int i=1; i<=sorted.length; i++){
                 if(ArrayUtils.contains(sorted, i)){
                     if(ArrayUtils.indexOf(sorted, i) != ArrayUtils.lastIndexOf(sorted, i)){
                         return false;
@@ -66,15 +66,20 @@ public class Board {
             }
         }
 
+        System.out.println(toString());
+
         // Validate columns
         for(int column=0; column<9; column++){
             int[] sorted = new int[]{};
             for(int row=0; row<9; row++) {
+                System.out.printf("row %d column %d  = %d\n", row, column, puzzleNums[row][column]);
                 sorted = ArrayUtils.add(sorted, puzzleNums[row][column]);
             }
+
+            System.out.println(Arrays.toString(sorted));
             Arrays.sort(sorted);
             sorted = ArrayUtils.removeAllOccurences(sorted, 0);
-            for(int i=1; i<=9; i++){
+            for(int i=1; i<=sorted.length; i++){
                 if(ArrayUtils.contains(sorted, i)){
                     if(ArrayUtils.indexOf(sorted, i) != ArrayUtils.lastIndexOf(sorted, i)){
                         return false;
@@ -84,9 +89,53 @@ public class Board {
         }
 
         // Validate grids
+        HashMap<Integer, Integer[][]> gridMapping = getGridHashMap();
+        for(int i=1; i<=9; i++){
+            Integer[][] addressMinMax = gridMapping.get(i);
+            int rowMin = addressMinMax[0][0];
+            int rowMax = addressMinMax[0][1];
+            int colMin = addressMinMax[1][0];
+            int colMax = addressMinMax[1][1];
+
+
+            // Build array from grid
+            int[] sorted = new int[]{};
+            for(int row=rowMin; row<=rowMax; row++){
+                for(int col=colMin; col<=colMax; col++){
+                    sorted = ArrayUtils.add(sorted, puzzleNums[row][col]);
+                }
+            }
+
+            Arrays.sort(sorted);
+            sorted = ArrayUtils.removeAllOccurences(sorted, 0);
+            for(int x=0; x<sorted.length; x++){
+                if(ArrayUtils.contains(sorted, i)){
+                    if(ArrayUtils.indexOf(sorted, i) != ArrayUtils.lastIndexOf(sorted, i)){
+                        return false;
+                    }
+                }
+            }
+        }
         
 
         return true;
+    }
+
+
+    private HashMap<Integer, Integer[][]> getGridHashMap(){
+        // Key=Grid Index     Value = [rowNum range][colNum range]
+        HashMap<Integer, Integer[][]> gridMap = new HashMap<>();
+        gridMap.put(1, new Integer[][]{{1,3},{1,3}});
+        gridMap.put(2, new Integer[][]{{1,3},{4,6}});
+        gridMap.put(3, new Integer[][]{{1,3},{7,9}});
+        gridMap.put(4, new Integer[][]{{4,6},{1,3}});
+        gridMap.put(5, new Integer[][]{{4,6},{4,6}});
+        gridMap.put(6, new Integer[][]{{4,6},{7,9}});
+        gridMap.put(7, new Integer[][]{{7,9},{1,3}});
+        gridMap.put(8, new Integer[][]{{7,9},{4,6}});
+        gridMap.put(9, new Integer[][]{{7,9},{7,9}});
+
+        return gridMap;
     }
 }
 
