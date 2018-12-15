7f0c883abb01484b84f62ef0b1557a07f9309345
Fine-ants <jordanhuusy@live.com>
2018-12-14 23:26:28 -0800

    added grid of TextFields

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,17 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main extends Application {
 
@@ -19,20 +26,26 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
 
-
         // Root node
-        buttonsGrid = new GridPane();
-        buttonsGrid.setAlignment(Pos.BOTTOM_CENTER);
-        buttonsGrid.setPadding(new Insets(0,0,15,0));
+        GridPane root = new GridPane();
+
 
         // Text boxes
         initSudokuBoard();
+        sudokuBoardGrid.setAlignment(Pos.TOP_CENTER);
+        sudokuBoardGrid.setPadding(new Insets(15,0,0,0));
+        root.add(sudokuBoardGrid, 0,0);
+
 
         // Buttons
         initButtons();
+        buttonsGrid.setAlignment(Pos.BOTTOM_CENTER);
+        buttonsGrid.setPadding(new Insets(0,0,15,0));
+        root.add(buttonsGrid, 0, 1);
+
 
         // Init window
-        Scene scene = new Scene(buttonsGrid, 300, 250);
+        Scene scene = new Scene(root, 300, 250);
         primaryStage.setTitle("Sudoku Solver");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -42,8 +55,33 @@ public class Main extends Application {
     // Initialize Sudoku board text boxes
     public void initSudokuBoard(){
 
-
+        // Buttons to grid
         sudokuBoardGrid = new GridPane();
+        TextField tf;
+        for(int row=0; row<9; row++) {
+            for(int col=0; col<9; col++) {
+
+
+                tf = new TextField();
+
+//                // Spacing for each Sudoku 3x3 grid
+//                if(row%3 == 0 && col%3 == 0) {
+//                    tf.setPadding(new Insets(0,5,5,0));
+//                }else if(row%3 == 0){
+//                    tf.setPadding(new Insets(0,0,5,0));
+//                }else if(col%3 == 0){
+//                    tf.setPadding(new Insets(0,5,0,0));
+//                }
+
+                sudokuBoardGrid.add(tf, col, row);
+            }
+        }
+
+
+        // Set grid widths
+        for(int i=0; i<9; i++) {
+            sudokuBoardGrid.getColumnConstraints().add(new ColumnConstraints(25));
+        }
 
     }
 
@@ -74,16 +112,11 @@ public class Main extends Application {
         });
 
 
-        // Set button constraints(location on grid)
-        GridPane.setConstraints(btnSolve, 0, 0);
-        GridPane.setConstraints(btnReset, 1, 0);
-        GridPane.setConstraints(btnClose, 2, 0);
-
-
         // Add child nodes
-        buttonsGrid.getChildren().add(btnReset);
-        buttonsGrid.getChildren().add(btnClose);
-        buttonsGrid.getChildren().add(btnSolve);
+        buttonsGrid = new GridPane();
+        buttonsGrid.add(btnSolve, 0, 0);
+        buttonsGrid.add(btnReset, 1, 0);
+        buttonsGrid.add(btnClose, 2, 0);
     }
 
 
